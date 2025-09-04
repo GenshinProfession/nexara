0b75bc6188238e946c9afa2cf54d2a5bfd5a4d08
LY-R <15538847229@163.com>
2025-09-04 23:37:21 +0800

    refactor(core): 添加PackageFactory支持多语言

--- nexara-server/src/main/java/com/nexara/server/core/code/PackageFactory.java
+++ nexara-server/src/main/java/com/nexara/server/core/code/PackageFactory.java
@@ -1,7 +1,29 @@
 package com.nexara.server.core.code;
 
+import com.nexara.server.core.docker.config.ServiceConfigGenerator;
+import com.nexara.server.polo.enums.CodeLanguage;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
+@Log4j2
 public class PackageFactory {
+    private final Map<CodeLanguage, PackageDetector> generatorMap = new ConcurrentHashMap<>();
+    public PackageFactory(List<PackageDetector> generators) {
+        for (PackageDetector generator : generators) {
+            generatorMap.put(generator.getLanguage(), generator);
+        }
+    }
+
+    public PackageDetector getGenerator(CodeLanguage language) {
+        PackageDetector generator = generatorMap.get(language);
+        if (generator == null) {
+            throw new IllegalArgumentException("Unsupported language: " + language);
+        }
+        return generator;
+    }
 }
